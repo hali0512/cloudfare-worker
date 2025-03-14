4af80dfa94dc4e1de554301b31db0dd3d4ad1172
Hussain Ali <hali@geeke.com>
2025-03-15 01:26:14 +0500

    extracting meatada from inccoming req

---
+++ worker-extracting-metadata/src/index.js
@@ -0,0 +1,16 @@
+// This Cloudflare Worker extracts and returns Cloudflare-specific metadata from the incoming request.
+
+export default {
+	async fetch(req) {
+	  const data =
+		req.cf !== undefined
+		  ? req.cf
+		  : { error: "The cf object is not available inside the preview." };
+  
+	  return new Response(JSON.stringify(data, null, 2), {
+		headers: {
+		  "content-type": "application/json;charset=UTF-8",
+		},
+	  });
+	},
+  };
